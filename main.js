d9ea916c002ff4adc7f1989971533e5c19326d15
vienvu <vien@vienvu.com>
2021-04-22 10:43:21 +0700

    1

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 AOS.init({
+  once: true,
   duration: 1000,
 });
 
 $(function () {
   $(".date").datepicker();
+
+  $("#button").click(function () {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#elementtoScrollToID").offset().top,
+      },
+      2000
+    );
+  });
 });
